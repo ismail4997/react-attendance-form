7cda6832357d16d4ca5fad49c45a40c6d2e1e810
ismail4997 <70561795+ismail4997@users.noreply.github.com>
2022-01-21 23:36:32 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import AttendanceForm from './components/AttendanceForm';
+
+function App() {
+  return (
+    <div className="Attendance-App">
+      <AttendanceForm/>
+     
+    </div>
+  );
+}
+
+export default App;
