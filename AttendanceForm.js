a636b8aafc0e839b5ed5240bec6c07ab2c927548
ismail4997 <70561795+ismail4997@users.noreply.github.com>
2022-01-24 21:25:08 +0500

    Add files via upload

--- AttendanceForm.js
+++ AttendanceForm.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 
 import './app1.css'
-class AttendanceForm extends Component {
-    constructor(props) {
+class AttendanceForm extends Component 
+{
+     constructor(props) 
+     {
         super(props);
-        this.state = { 
+        this.state = 
+        { 
             name: "",
             status: "",
             id: "",
             option: "",
-         };
-         this.changeNameHandler=this.changeNameHandler.bind(this);
-         this.changeIdHandler=this.changeIdHandler.bind(this);
-         this.changeOptionHandler=this.changeOptionHandler.bind(this);
+        };
+        // we are using this function in this constructor so we can callback this function 
+        // without the fear that it will lose its context
+        this.changeNameHandler=this.changeNameHandler.bind(this);
+        this.changeIdHandler=this.changeIdHandler.bind(this);
+        this.changeOptionHandler=this.changeOptionHandler.bind(this);
 
     }
-
-    changeNameHandler=(event)=>{
+    // these functions save the data into the their respective ids 
+    changeNameHandler=(event)=>
+    {
         this.setState({name:event.target.value});
 
     }
-    changeIdHandler=(event)=>{
+    changeIdHandler=(event)=>
+    {
         this.setState({id:event.target.value});
     }
     changeOptionHandler=(event)=>
@@ -28,63 +35,68 @@ class AttendanceForm extends Component {
         this.setState({option:event.target.value})
     }
 
-    saveStudent=(e)=>{
+    // this func saving data into the student array and showing into the console
+    saveStudent=(e)=>
+    {
         e.preventDefault();
         let student={name: this.state.name,id: this.state.id,option: this.state.option};
         console.log('Student=>'+JSON.stringify(student));
     }
 
-    cancel(){
+    cancel()
+    {
+        // adds an entry into the history stack
         this.props.history.push();
     }
-    render() { 
+    render() 
+    { 
         return ( 
             <div>
                 <div className='container'>
                     <h1 className='text' > Student AttendanceForm </h1>
-
                     <form className='Attendance-form' onSubmit={this.handleSubmit} >
-                        <div className='first'>
-                            <input 
+                        {/* a separate div for the first input enter student name*/}
+                        <div className='first'> 
+                            <input
                             type="text" 
                             placeholder='Student name' 
                             value={this.state.name} 
                             name='text'
-                            onChange={this.changeNameHandler}
+                            onChange={this.changeNameHandler} //handling name into this when the data is entered
                             className='Attendance-Input'/>
                            
-                            </div>
-                            <br></br>
-                            <div className='second'>
-                                <input
-                                type="text" 
-                                placeholder='Enter Reg Id' 
-                                value={this.state.id } 
-                                name='text'
-                                onChange={this.changeIdHandler}
-                                className='Attendance-Input'/>
-                            </div>
-                            <br></br>
-                            <div onChange={this.changeOptionHandler} class="third">
-                                <input type="radio" value="Present" name="class" /> Present
-                                <input type="radio" value="Absent" name="class" /> Absent
-                                <input type="radio" value="Late" name="class" /> Late
-                            </div>
+                        </div>
+                        <br></br>
+                        {/* a separate div for the first input enter student id*/}
+                        <div className='second'>
+                            <input
+                            type="text" 
+                            placeholder='Enter Reg Id' 
+                            value={this.state.id } 
+                            name='text'
+                            onChange={this.changeIdHandler} //handling id into this when the data is entered
+                            className='Attendance-Input'/>
+                        </div>
 
-                            <div>
-                                <button className="success" onClick={this.saveStudent}>Submit the details</button>
-                                <button className="danger" onClick={this.cancel.bind}> Reset </button> 
-                            </div>
-                            
+                        <br></br>
+                        {/* a separate div for the first input enter student presence*/}
+                        <div onChange={this.changeOptionHandler} class="third">  
+                        {/* giving the handler function to control the context for the option id  */}
+                            <input type="radio" value="Present" name="class" /> Present
+                            <input type="radio" value="Absent" name="class" /> Absent
+                            <input type="radio" value="Late" name="class" /> Late
+                        </div>
 
-            </form>
+                        <div>
+                            <button className="success" onClick={this.saveStudent}>Submit the details</button> 
+                            {/* calling the function for the saving details  */}
+                            <button className="danger" onClick={this.cancel.bind}> Reset </button>
+                            {/* pushing the data into the history stack   */}
+                        </div>
+                    </form>
+                </div>
             </div>
-            </div>
-            
-           
-
-           
-         );
+        );
     }
 }
  
